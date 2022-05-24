a14f3da90bbf6beda8260fca9f518aeb38690130
solomonbarayev <sbarayev2@gmail.com>
2022-05-25 00:05:14 +0300

    Add gallery module with swiperJS

---
+++ scripts/swiper.js
@@ -0,0 +1,18 @@
+let swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+let swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
