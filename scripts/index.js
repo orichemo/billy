b7dc16b739b87e1b2b8834b57208c58ddb8fc2e4
Ori Chemo <orichemo@gmail.com>
2022-05-25 16:21:40 +0300

    solomon i will kill you again

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,16 @@
-
-
 const menuButton = document.querySelector(".nav__hmburger-button");
-const menuLinks = document.querySelector(".nav__links")
-const linksList = Array.from(menuLinks.querySelectorAll(".nav__link"))
-const barOne = document.querySelector(".nav__bar_rotate_plus")
-const barTwo = document.querySelector(".nav__bar_opacity")
-const varThree = document.querySelector(".nav__bar_rotate_minus")
-console.log(linksList)
+const menuLinks = document.querySelector(".nav__links");
+const linksList = Array.from(menuLinks.querySelectorAll(".nav__link"));
+const navBar = Array.from(document.querySelectorAll(".nav__bar"));
 
 toggleMenuButton = () => {
   menuLinks.classList.toggle("nav__links_active");
-  barOne.classList.toggle("nav__bar_rotate_plus_active");
-  barTwo.classList.toggle("nav__bar_opacity_active");
-  varThree.classList.toggle("nav__bar_rotate_minus_active");
-}
+  navBar.forEach((bar) => {
+    bar.classList.toggle(`${bar.classList[1]}_active`);
+  });
+};
 
-
-
-menuButton.addEventListener("click", () => toggleMenuButton())
-
+menuButton.addEventListener("click", () => toggleMenuButton());
 linksList.forEach((link) => {
-    link.addEventListener("click", () => toggleMenuButton())
+  link.addEventListener("click", () => toggleMenuButton());
 });
